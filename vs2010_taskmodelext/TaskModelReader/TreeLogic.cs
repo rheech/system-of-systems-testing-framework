4a3590f17aed1906949100dae6fd9d0e404ca10a
rheech <lee@cheonghyun.com>
2016-04-06 10:29:26 +0900

    Add tree sequence

---
+++ vs2010_taskmodelext/TaskModelReader/TreeLogic.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Xml;
+
+namespace TaskModelReader
+{
+    class TreeLogic
+    {
+        public void add(XmlNode node)
+        {
+        }
+
+        public void combine(XmlNode node)
+        {
+        }
+
+        public void split(XmlNode node)
+        {
+        }
+
+    }
+}
