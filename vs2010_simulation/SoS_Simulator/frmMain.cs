b57776d7ddd0388832e245de0e421edab72e3832
rheech <lee@cheonghyun.com>
2016-06-02 13:54:59 +0900

    Modify xml

--- vs2010_simulation/SoS_Simulator/frmMain.cs
+++ vs2010_simulation/SoS_Simulator/frmMain.cs
@@ -49,7 +49,7 @@ namespace SoS_Simulator
         private void frmMain_Load(object sender, EventArgs e)
         {
             InitializeSimulatorDisplay();
-            //LoadDefault();
+            LoadDefault();
         }
 
         private void lstGoals_SelectedIndexChanged(object sender, EventArgs e)
